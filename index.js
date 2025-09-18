d35f5eb95a831d736eda153abd0be69ce13400d9
Ankit Shrestha <ankitshrestha@Ankits-MacBook-Air.local>
2025-09-18 13:06:12 +1000

    added delete option

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { dirname } from "path";
+import { dirname, parse } from "path";
 import { fileURLToPath } from "url";
 
 
@@ -64,6 +64,46 @@ app.get("/post/:id", (req, res) => {
 
     res.render(__dirname+"/views/post.ejs", {post});
 });
+
+app.post("/update-post/:id", upload.single('changeBlogImage'), (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const postIndex = posts.findIndex(p => p.id === postId);
+
+    if(postIndex === -1){
+        return res.status(404).send('page not found');
+    }
+
+    const {firstName, lastName, email, content, date, changeImage} = req.body;
+    const changeBlogImage = req.file;
+
+    console.log(req.body);
+    console.log("change blog image: ", changeBlogImage);
+    posts[postIndex].firstName = firstName;
+    posts[postIndex].lastName = lastName;
+    posts[postIndex].email = email;
+    posts[postIndex].content = content;
+    posts[postIndex].date = date;
+    
+    if(changeImage === "yes" && changeBlogImage){
+        posts[postIndex].blogImage = changeBlogImage.filename;
+    }
+
+    res.redirect("/?message=post+updated+successfully");
+});
+
+app.post("/delete-post/:id", (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const postIndex = posts.findIndex(p => p.id === postId);
+
+    if (postIndex === -1){
+        return res.status(404).send("post not found")
+    }
+
+    posts.splice(postIndex, 1);
+    res.status(200).send("post deleted");
+});
+
+
 
 app.listen(port, () =>{
     console.log(`server running on port number ${port}`);
--- public/js/script.js
+++ public/js/script.js
@@ -6,28 +6,114 @@ const overlayModal = document.querySelector("#overlay-modal");
 const modalBox = document.querySelector("#modal-box");
 const checkoutBtn = document.querySelector("#checkout-btn");
 const backtoTopBtn = document.querySelector("#back-to-top-btn");
+const editPostBtn = document.querySelector("#edit-post-btn");
+const editpostPopupModal = document.querySelector("#editpost-popup-modal");
+const editpostOverlayModal= document.querySelector("#editpost-overlay-modal");
+const editpostCloseBtn = document.querySelector("#edit-post-close-btn");
+const changeImageRadios = document.querySelectorAll("input[name='changeImage']");
+const changeBlogimageContainer = document.getElementById("changeBlogimage-container");
+const delPostBtn = document.getElementById("del-post-btn");
 
+if(backtoTopBtn){
 backtoTopBtn.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth" 
     });
-})
+});}
 
+if(editPostBtn){
+    editPostBtn.addEventListener("click", (e) => {
+        editpostPopupModal.classList.add("active");
+
+        const button = e.currentTarget;
+        
+        const firstName = button.getAttribute('data-firstName');
+        const lastName = button.getAttribute('data-lastName')
+        const email = button.getAttribute('data-email')
+        const content = button.getAttribute('data-content')
+        const image = button.getAttribute('data-image')
+
+        document.getElementById('firstName').value = firstName;
+        document.getElementById('lastName').value = lastName;
+        document.getElementById('email').value = email;
+        document.getElementById('content').value = content;
+        document.getElementById('editpost-image-preview').src = `/uploads/${image}`;
+
+
+    })
+}
+
+if(delPostBtn){
+    delPostBtn.addEventListener("click", async() => {
+        const postId = delPostBtn.dataset.id;
+
+        if(!confirm("Are you Sure want to del the post?"))return;
+
+        try{
+            const response = await fetch(`/delete-post/${postId}`, {
+                method: "POST",
+            });
+
+            if(response.ok){
+                console.log("post deleted successfully");
+
+                alert("✅ Post deleted successfully!");
+                window.location.href = "/";
+            }else{
+                console.error("failed to delete post");
+            }
+        
+        }catch (error){
+            console.error("error deleteing post", error);
+        }
+    });
+}
+
+if(editpostOverlayModal){
+    editpostOverlayModal.addEventListener("click",(e) =>{
+        if (e.target == editpostOverlayModal) {
+            editpostPopupModal.classList.remove("active");
+        }
+    });}
+    
+if(editpostCloseBtn){
+    editpostCloseBtn.addEventListener("click",() =>{
+        editpostPopupModal.classList.remove("active")
+});}
+
+
+
+
+if(overlayBtn){
 overlayBtn.addEventListener("click", (e) => {  
     popupModal.classList.add("active");
    
-});
+});}
 
+if(overlayModal){
 overlayModal.addEventListener("click",(e) =>{
     if (e.target == overlayModal) {
         popupModal.classList.remove("active");
     }
-});
+});}
 
+if(closeBtn){
 closeBtn.addEventListener("click",() =>{
     popupModal.classList.remove("active")
-});
+});}
+
+if(changeImageRadios){
+    changeImageRadios.forEach(radio => {
+        radio.addEventListener("change", ()=>{
+            if(radio.value === 'yes' && radio.checked){
+                changeBlogimageContainer.style.display = 'block';
+            }else if(radio.value === 'no' && radio.checked){
+                changeBlogimageContainer.style.display = 'none';
+            }
+        })
+    })
+}
 
 
 
